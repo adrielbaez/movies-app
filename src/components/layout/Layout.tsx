d0dbc9e615bde89ec359453d308bbac37e5ff656
Adriel Baez <baez.adriel27@gmail.com>
2023-02-12 16:20:07 -0300

    seo

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import React from "react";
 import { Footer } from "./Footer";
 import { NavBar } from "./NavBar";
@@ -7,19 +8,31 @@ interface LayoutProps {
   children: JSX.Element | JSX.Element[];
   showNav?: boolean;
   movieId: number;
+  title?: string;
+  description?: string;
 }
 
 const Layout: NextPage<LayoutProps> = ({
   children,
   showNav = true,
   movieId,
+  title,
+  description,
 }) => {
   return (
-    <div className="bg-black min-h-screen">
-      {showNav && <NavBar movieId={movieId} />}
-      <>{children}</>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>{title || "Movies - App"}</title>
+        <meta name="author" content="Adriel Baez" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={`${title || ""}, movies`} />
+      </Head>
+      <div className="bg-black min-h-screen">
+        {showNav && <NavBar movieId={movieId} />}
+        <>{children}</>
+        <Footer />
+      </div>
+    </>
   );
 };
 
